0802d5b3ee66788da9ffbbf65de3a3846f94902f
tordragon <tordragonnn@gmail.com>
2018-02-21 11:49:01 +0700

    add complete

--- app/src/main/java/sdu/cs58/torsak/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/torsak/calculator/MainActivity.java
@@ -2,12 +2,43 @@ package sdu.cs58.torsak.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //1 Expiicit
+    EditText number1EditText, number2EdiText;
+    Button addButton;
+    TextView resultTextView;
+    int num1, num2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Initial View ผูกตัวแปับน Java กับอิลมันท์บน XML
+        number1EditText = findViewById(R.id.editText1);
+        number2EdiText = findViewById(R.id.editText2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //แปลงค่าที่รับจาก EditText เป็นตัวเลข แล้วก็เก็บลงในตัวแปรชนิด int
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EdiText.getText().toString().trim());
+                result = num1 + num2;
+
+                //4. แสดงผลลับที่กำหนด
+                resultTextView.setText(result + "");
+            }
+        });//setOnClickListener
+
+    }//end Method Creste
+
+}//end Ciass
